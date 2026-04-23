7c041195162452a7b38c59515a110d97d416515b
code wizard <codewizard@gmail.com>
2026-04-23 22:16:28 +0530

    create ScrollToTopButton

---
+++ src/components/ScrollToTopButton.jsx
@@ -0,0 +1,33 @@
+import { useEffect, useState } from "react";
+import { LuArrowUp } from "react-icons/lu";
+
+export default function ScrollToTopButton() {
+  const [show, setShow] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      setShow(window.scrollY > 300); // 300px ke baad show hoga
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+
+  const scrollToTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
+  };
+
+  if (!show) return null;
+
+  return (
+    <button
+      onClick={scrollToTop}
+      className="fixed bottom-6 right-6 z-50 bg-cyan-500 hover:bg-cyan-600 text-white p-3 rounded-full shadow-lg transition"
+    >
+      <LuArrowUp size={20} />
+    </button>
+  );
+}
--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import Navbar from "../components/Navbar";
 import Sidebar from "../components/Sidebar";
 import Footer from "../components/Footer";
+import ScrollToTopButton from "../components/ScrollToTopButton";
 
 const MainLayout = ({ children }) => {
   const [open, setOpen] = useState(false);
@@ -9,18 +10,18 @@ const MainLayout = ({ children }) => {
   return (
     <div className="flex flex-col min-h-screen">
 
-      {/* Navbar */}
+     
       <Navbar setOpen={setOpen} />
 
-      {/* Sidebar */}
+     
       <Sidebar open={open} setOpen={setOpen} />
 
-      {/* Main Content */}
+  
       <main className="flex-1 pt-24">
         {children}
       </main>
 
-      {/* Footer always bottom */}
+      <ScrollToTopButton />  
       <Footer />
 
     </div>
