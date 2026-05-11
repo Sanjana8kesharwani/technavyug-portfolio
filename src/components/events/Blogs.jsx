a3a98ecbb3ab9ac6563d58472c18ab5a1cb2aa99
Sanjana Kesharwani <sanjanakesharwani32@gmail.com>
2026-05-11 17:55:34 +0530

    fix respsosive in Blogs page

--- src/components/events/Blogs.jsx
+++ src/components/events/Blogs.jsx
@@ -5,12 +5,24 @@ export default function Blogs() {
   const navigate = useNavigate();
 
   return (
-    <div className="space-y-8">
+    <div className="space-y-6 sm:space-y-8 overflow-x-hidden">
       {blogsData.map((blog) => (
-        <div key={blog.id}>
-          <h3>{blog.title}</h3>
+        <div
+          key={blog.id}
+          className="w-full break-words"
+        >
+          <h3 className="text-lg sm:text-xl font-semibold text-gray-900 leading-snug">
+            {blog.title}
+          </h3>
 
-          <span onClick={() => navigate(`/blog/${blog.id}`, { state: blog })} className="text-blue-600 cursor-pointer">
+          <span
+            onClick={() =>
+              navigate(`/blog/${blog.id}`, {
+                state: blog,
+              })
+            }
+            className="inline-block mt-2 text-sm sm:text-base text-blue-600 cursor-pointer hover:text-blue-700 transition break-words"
+          >
             Read More...
           </span>
         </div>
